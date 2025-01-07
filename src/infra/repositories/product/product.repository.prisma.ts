1da375cdc9352fdd39c13109e7027987486aa0cd
LucasMagnun <120067757+LucasMagnun@users.noreply.github.com>
2025-01-07 16:05:19 -0300

    configurando tabela de produtos no prisma

--- src/infra/repositories/product/product.repository.prisma.ts
+++ src/infra/repositories/product/product.repository.prisma.ts
@@ -11,9 +11,32 @@ export class ProductRepositoryPrisma implements ProductGateway {
   }
 
   public async save(product: Product): Promise<void> {
-    throw new Error("Não implementado!")
+    const data =  {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity
+    }
+
+    await this.prismaClient.product.create({
+      data
+    });
   }
+
   public async list(): Promise<Product[]> {
-    throw new Error("Não implementado!")
+    const products = await this.prismaClient.product.findMany();
+
+    const productList = products.map((p) => {
+      const product = Product.with({
+        id: p.id,
+        name: p.name,
+        price:  p.price,
+        quantity: p.quantity
+      })
+
+      return product
+    })
+    
+    return productList
   }
 }
